43869020807cb44e123a9a2d9a28c340b0a2d245
woojn <729344719@qq.com>
2017-08-22 18:13:50 +0800

    欢迎页

---
+++ app/src/main/java/cn/woojn/vedioinkotlin/ui/WelComeActivity.kt
@@ -0,0 +1,14 @@
+package cn.woojn.vedioinkotlin.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by wujun on 17-8-22.
+ */
+class WelComeActivity:AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
